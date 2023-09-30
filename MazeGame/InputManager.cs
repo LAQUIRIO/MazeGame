bb9f32beb33c322dead49b61a1d7bbd0a6e1c800
clovis boucher <clovis.boucher@dawsoncollege.qc.ca>
2023-09-30 19:00:07 -0400

    add key action lists

---
+++ MazeGame/InputManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public delegate void Action();
+
+namespace MazeGame
+{
+    public sealed class InputManager
+    {
+        private static List<(char,Action)> values = new List<(char, Action)>();
+        private static InputManager instance = null;
+        public static InputManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new InputManager();
+                }
+                return instance;
+            }
+        }
+        public void AddKeyHandler(char key,Action action)
+        {
+            if (instance != null) { 
+                values.Add((key,action));
+            }
+        }
+    }
+}
