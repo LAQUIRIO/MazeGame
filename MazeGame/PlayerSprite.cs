afa6983a5283785ed52c5e36da47b618eb582d5e
clovis boucher <clovis.boucher@dawsoncollege.qc.ca>
2023-09-30 19:23:17 -0400

    add ctor for playerSprite

---
+++ MazeGame/PlayerSprite.cs
@@ -0,0 +1,30 @@
+﻿using Maze;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MazeGame
+{
+    internal class PlayerSprite : DrawableGameComponent
+    {
+        private IPlayer _player;
+        private SpriteBatch _spriteBatch;
+        private Game _game;
+        private Texture2D _PlayerTexture;
+
+        public PlayerSprite(Game game) : base(game)
+        {
+            if (game == null)
+            {
+                throw new ArgumentNullException(nameof(game));
+            }
+            _game = game;
+        }
+
+    }
+}
