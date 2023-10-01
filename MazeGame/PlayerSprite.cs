21ab6411b6cabf94d2f716772575ce9faca0e86f
clovis boucher <clovis.boucher@dawsoncollege.qc.ca>
2023-10-01 11:17:34 -0400

    add the use of inputManager

--- MazeGame/PlayerSprite.cs
+++ MazeGame/PlayerSprite.cs
@@ -17,34 +17,47 @@ namespace MazeGame
         private SpriteBatch _spriteBatch;
         private Game _game;
         private Texture2D _PlayerTexture;
+        private InputManager _inputManager;
+        private int _imgSize = 32;
 
-        public PlayerSprite(Game game) : base(game)
+        public PlayerSprite(Game game, IPlayer player) : base(game)
         {
             if (game == null)
             {
                 throw new ArgumentNullException(nameof(game));
             }
             _game = game;
+            _player = player;
         }
         public override void Initialize()
         {
+            _spriteBatch = new SpriteBatch(_game.GraphicsDevice);
+            _inputManager = InputManager.Instance;
+            _inputManager.AddKeyHandler(Keys.Up, _player.MoveForward);
+            _inputManager.AddKeyHandler(Keys.Down, _player.MoveBackward);
+            _inputManager.AddKeyHandler(Keys.Left, _player.TurnLeft);
+            _inputManager.AddKeyHandler(Keys.Right, _player.TurnRight);
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
+            _texture = _game.Content.Load<Texture2D>("LadyBug");
             base.LoadContent();
         }
 
         public override void Update(GameTime gameTime)
         {
+            _inputManager.Update();
             base.Update(gameTime);
         }
 
         public override void Draw(GameTime gameTime)
         {
             _spriteBatch.Begin();
-            _spriteBatch.Draw(_texture, new Vector2(_player.StartX, _player.StartY), Color.White;
+            Vector2 vector2 = new Vector2(_player.StartX * _imgSize, _player.StartY * _imgSize);
+            Vector2 center = new Vector2(_texture.Width/2,_texture.Height/2);
+            _spriteBatch.Draw(_texture, vector2, null,Color.White,_player.GetRotation(),center,1,SpriteEffects.None,1);
             _spriteBatch.End();
             base.Draw(gameTime);
 
