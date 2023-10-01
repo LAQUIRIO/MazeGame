c17278a0a45fbf64103d07c8f0dec53a19ccfff8
clovis boucher <clovis.boucher@dawsoncollege.qc.ca>
2023-09-30 23:40:07 -0400

    change key var type to keys

--- MazeGame/InputManager.cs
+++ MazeGame/InputManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.Xna.Framework.Input;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ namespace MazeGame
 {
     public sealed class InputManager
     {
-        private static List<(char,Action)> keys = new List<(char, Action)>();
+        private static List<(Keys, Action)> keys = new List<(Keys, Action)>();
         private static InputManager instance = null;
         public static InputManager Instance
         {
@@ -22,23 +23,23 @@ namespace MazeGame
                 return instance;
             }
         }
-        public static void update()
+        public static void Update()
         {
             if (instance != null) { 
                 keys.ForEach((key) =>
                 {
                     if (Console.KeyAvailable)
                     {
-                        ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                        /*ConsoleKeyInfo keyInfo = Console.ReadKey(true);
                         if (keyInfo.KeyChar == key.Item1)
                         {
                             key.Item2();
-                        }
+                        }*/
                     }
                 }); 
             }
         }
-        public static void AddKeyHandler(char key,Action action)
+        public static void AddKeyHandler(Keys key,Action action)
         {
             if (instance != null) { 
                 keys.Add((key,action));
