6deb3655368aa183ae645a77e9040734a73d78a3
clovis boucher <clovis.boucher@dawsoncollege.qc.ca>
2023-10-03 19:43:45 -0400

    add main method and program class

--- MazeGame/Program.cs
+++ MazeGame/Program.cs
@@ -1,3 +1,14 @@
 ﻿
-using var game = new MazeGame.MazeGame();
-game.Run();
+using System;
+using System.Drawing.Text;
+
+internal class Program
+{
+    [STAThreadAttribute]
+    public static void Main()
+    {
+        using var game = new MazeGame.MazeGame();
+        game.Run();
+
+    }
+}
