b0b61cba3b44b395c77caec2070965134eabcd33
clovis boucher <clovis.boucher@dawsoncollege.qc.ca>
2023-10-03 09:15:20 -0400

    make player only redraw if player moved

--- MazeGame/PlayerSprite.cs
+++ MazeGame/PlayerSprite.cs
@@ -12,6 +12,8 @@ namespace MazeGame
         private readonly Logger logging = LogManager.GetCurrentClassLogger();
         private IPlayer _player;
         private Texture2D _texture;
+        private Texture2D _floor;
+        private Vector2 _oldPosition;
         private SpriteBatch _spriteBatch;
         private readonly Game _game;
         private InputManager _inputManager;
@@ -33,30 +35,42 @@ namespace MazeGame
             _inputManager.AddKeyHandler(Keys.Down, _player.MoveBackward);
             _inputManager.AddKeyHandler(Keys.Left, _player.TurnLeft);
             _inputManager.AddKeyHandler(Keys.Right, _player.TurnRight);
+            _oldPosition = new Vector2(_player.Position.X, _player.Position.Y);
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
+            _floor = _game.Content.Load<Texture2D>("path");
             _texture = _game.Content.Load<Texture2D>("LadyBug");
             base.LoadContent();
         }
 
         public override void Update(GameTime gameTime)
         {
-            _inputManager.Update();
+            if (!_inputManager.playerMoved)
+            {
+                _inputManager.Update();
+
+            }
             base.Update(gameTime);
         }
 
         public override void Draw(GameTime gameTime)
         {
-            logging.Debug($"Draw player at {_player.Position.X},{_player.Position.Y} looking {_player.Facing}");
-            _spriteBatch.Begin();
-            Vector2 vector2 = new Vector2(getStartingPoint(_player.Position.X, _texture.Width), getStartingPoint(_player.Position.Y, _texture.Height));
-            Vector2 center = new Vector2(_texture.Width / 2, _texture.Height / 2);
-            _spriteBatch.Draw(_texture, vector2, null, Color.White, _player.GetRotation(), center, 1, SpriteEffects.None, 1);
-            _spriteBatch.End();
-            base.Draw(gameTime);
+            if (_inputManager.playerMoved)
+            {
+                logging.Debug($"Draw player at {_player.Position.X},{_player.Position.Y} looking {_player.Facing}");
+                _spriteBatch.Begin();
+                Vector2 vector2 = new Vector2(getStartingPoint(_player.Position.X, _texture.Width), getStartingPoint(_player.Position.Y, _texture.Height));
+                Vector2 center = new Vector2(_texture.Width / 2, _texture.Height / 2);
+                _spriteBatch.Draw(_floor, new Vector2(_oldPosition.X * _floor.Width, _oldPosition.Y * _floor.Height), Color.White);
+                _oldPosition = new Vector2(_player.Position.X, _player.Position.Y);
+                _spriteBatch.Draw(_texture, vector2, null, Color.White, _player.GetRotation(), center, 1, SpriteEffects.None, 1);
+                _inputManager.playerMoved = false;
+                _spriteBatch.End();
+                base.Draw(gameTime);
+            }
         }
         private float getStartingPoint(int position, int imgSize)
         {
