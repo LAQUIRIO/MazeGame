b1701ad211e04883566043656e97be61266f8b41
clovis boucher <clovis.boucher@dawsoncollege.qc.ca>
2023-10-03 20:01:27 -0400

    add try catch to log unexpected exception

--- MazeGame/Program.cs
+++ MazeGame/Program.cs
@@ -1,14 +1,24 @@
 ﻿
+using NLog;
 using System;
 using System.Drawing.Text;
 
 internal class Program
 {
+    private static readonly Logger logger = LogManager.GetCurrentClassLogger();
     [STAThreadAttribute]
     public static void Main()
     {
-        using var game = new MazeGame.MazeGame();
-        game.Run();
+        try//try catch is to log any exception
+        {
+            using var game = new MazeGame.MazeGame();
+            game.Run();
+        }
+        catch (Exception e)
+        {
+            logger.Debug("program encountered an exception:"+e);
+            throw;
+        }
 
     }
 }
