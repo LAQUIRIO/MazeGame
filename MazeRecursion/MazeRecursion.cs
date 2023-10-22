14e5303e26290a8c0340f4d05573be2f25ab801a
clovis boucher <clovis.boucher@dawsoncollege.qc.ca>
2023-10-22 17:10:29 -0400

    implement mapProvider

--- MazeRecursion/MazeRecursion.cs
+++ MazeRecursion/MazeRecursion.cs
@@ -1,43 +1,35 @@
 ﻿namespace MazeRecursion;
 using Maze;
-public class MazeRecursion
+public class MazeRecursion : IMapProvider
 {
-    private Direction[,] _directions;
-    private List<Direction> _path;
-    private Random _rand = new Random();
-    private MapVector _startingVector;
-    public MazeRecursion(int width, int height, MapVector currentVector)
+    private Direction[,]? _directions;
+    private List<MapVector> _path;
+    private readonly Random _rand = new Random();
+    private MapVector? _startingVector;
+    public MazeRecursion(MapVector? startingVector)
     {
-        _directions = new Direction[width, height];
-        _path = new List<Direction>();
-        if (currentVector == null)
-        {
-            int x = _rand.Next(0, width);
-            int y = _rand.Next(0, height);
-            currentVector = new MapVector(x, y);
-        }
-        _startingVector = currentVector;
+        _path = new List<MapVector>();
+        _startingVector = startingVector;
     }
      
-        public void Walk(MapVector providedVect)
+    private void Walk(MapVector providedVect)
     {
-        if (_directions.Length <= _path.Count)
+        if (_directions == null ||_directions.Length <= _path.Count)
         {
             return;
         }
-        while (true)//avoiding trapping itself in a corner and ending the gen early
-        { 
             List<Direction> dirs = GetPossibleDirections(providedVect);
-            if (dirs.Count == 0)
-            {
-                return;
-            }
+
+        while (dirs.Count>0)//avoiding trapping itself in a corner and ending the gen early
+        {   
             Direction dir = dirs[_rand.Next(0, dirs.Count)];
             _directions[providedVect.X, providedVect.Y] |= dir;
             MapVector newVect = providedVect + dir;
             _directions[newVect.X, newVect.Y] |= GetOppositeDir(dir);
-        }
+            Walk(newVect);
 
+            dirs = GetPossibleDirections(providedVect);
+        }
     }
 
     private static Direction GetOppositeDir(Direction dir)
@@ -55,6 +47,44 @@ public class MazeRecursion
 
     private List<Direction> GetPossibleDirections(MapVector providedVect)
     {
-        throw new NotImplementedException();
+        Direction[] directions = (Direction[])Enum.GetValues(typeof(Direction));
+        List<Direction> result = new List<Direction>();
+        foreach (var dir in directions)
+        {
+            if (_path.Contains(providedVect + dir))
+            {
+                result.Add(dir);
+            }
+        }
+        return result;
+    }
+
+    public Direction[,] CreateMap(int width, int height)
+    {
+        _directions = new Direction[width, height];
+        if (_startingVector == null)
+        {
+            int x = _rand.Next(0, width);
+            int y = _rand.Next(0, height);
+            _startingVector = new MapVector(x, y);
+        }
+        Walk(_startingVector);
+        return _directions;
+    }
+
+    public Direction[,] CreateMap()
+    {
+        int width = 5;
+        int height = 5;
+        _directions = new Direction[width, height];
+
+        if (_startingVector == null)
+        {
+            int x = _rand.Next(0, _directions.GetLength(0));
+            int y = _rand.Next(0, _directions.GetLength(1));
+            _startingVector = new MapVector(x, y);
+        }
+        Walk(_startingVector);
+        return _directions;
     }
 }
