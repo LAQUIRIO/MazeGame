e6f6748740780fff4016a59985c68394d032486e
clovis boucher <clovis.boucher@dawsoncollege.qc.ca>
2023-10-22 15:58:49 -0400

    add recursion project

---
+++ MazeRecursion/MazeRecursion.cs
@@ -0,0 +1,16 @@
+﻿namespace MazeRecursion;
+using Maze;
+public class MazeRecursion
+{
+    Direction[,] directions;
+    List<Direction> path;
+    public MazeRecursion(int width, int height)
+    {
+        directions = new Direction[width, height];
+        path = new List<Direction>();
+    }
+    void Walk(MapVector providedVect)
+    {
+
+    }
+}
