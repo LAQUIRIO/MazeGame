47a7864b725482d096dbf1f227fd08c891e7b16c
clovis boucher <clovis.boucher@dawsoncollege.qc.ca>
2023-10-23 09:03:40 -0400

    add test project with generated file

---
+++ MazeRecursionTests/MazeGeneratorTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MazeRecursion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeRecursion.Tests
+{
+    [TestClass()]
+    public class MazeGeneratorTests
+    {
+        [TestMethod()]
+        public void MazeGeneratorTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateMapTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateMapTest1()
+        {
+            Assert.Fail();
+        }
+    }
+}
