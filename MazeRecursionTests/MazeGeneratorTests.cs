5f77f0c6655347926141e7e457601efdb37c2912
clovis boucher <clovis.boucher@dawsoncollege.qc.ca>
2023-10-23 10:10:19 -0400

    implement test createMap()

--- MazeRecursionTests/MazeGeneratorTests.cs
+++ MazeRecursionTests/MazeGeneratorTests.cs
@@ -5,12 +5,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Maze;
 
 namespace MazeRecursion.Tests
 {
     [TestClass()]
     public class MazeGeneratorTests
     {
+        private IMapProvider _mapProvider = new MazeGenerator(null, null);
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            MapVector vector = new MapVector(0, 0);
+            _mapProvider = new MazeGenerator(vector, 1);
+        }
         [TestMethod()]
         public void MazeGeneratorTest()
         {
@@ -20,13 +28,33 @@ namespace MazeRecursion.Tests
         [TestMethod()]
         public void CreateMapTest()
         {
-            Assert.Fail();
+            Direction[,] ExpectedMaze = new Direction[,]
+            {
+                {Direction.S, Direction.E|Direction.S, Direction.E|Direction.W, Direction.S|Direction.W, Direction.S},
+                {Direction.N|Direction.E, Direction.N|Direction.W, Direction.S, Direction.N|Direction.E, Direction.N|Direction.S|Direction.W},
+                {Direction.E|Direction.S, Direction.E|Direction.S|Direction.W, Direction.N|Direction.W, Direction.E|Direction.S, Direction.N|Direction.W},
+                {Direction.N|Direction.S, Direction.N|Direction.E, Direction.S|Direction.W, Direction.N|Direction.E, Direction.S|Direction.W},
+                {Direction.N|Direction.E, Direction.W, Direction.N|Direction.E, Direction.W|Direction.E, Direction.N|Direction.W }
+            };
+            int expectedArraySizes = 5;
+            Direction[,] directions = _mapProvider.CreateMap();
+            Assert.AreEqual(expectedArraySizes, directions.GetLength(0));
+            Assert.AreEqual(expectedArraySizes, directions.GetLength(1));
+            for (int i = 0; i < expectedArraySizes; i++)
+            {
+                for (int j = 0; j < expectedArraySizes; j++)
+                {
+                    Assert.AreEqual(ExpectedMaze[i, j], directions[i, j]);
+                }
+            }
+
+
         }
 
         [TestMethod()]
         public void CreateMapTest1()
         {
-            Assert.Fail();
+            Direction[,] directions = _mapProvider.CreateMap(10, 10);
         }
     }
 }
