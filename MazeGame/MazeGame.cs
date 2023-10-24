3de1501d46380b096a7579101b8fe60ef2edbd78
clovis boucher <clovis.boucher@dawsoncollege.qc.ca>
2023-10-24 19:03:02 -0400

    extracting maze into its own class

--- MazeGame/MazeGame.cs
+++ MazeGame/MazeGame.cs
@@ -1,10 +1,10 @@
 ﻿using Maze;
+using MazeRecursion;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Media;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using NLog;
-using System.Drawing;
-using System.IO;
 using System.Windows.Forms;
 using ButtonState = Microsoft.Xna.Framework.Input.ButtonState;
 using Color = Microsoft.Xna.Framework.Color;
@@ -18,11 +18,16 @@ public class MazeGame : Game
     private readonly GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
     private IMap _map;
+    private MediaState gameState;
     private PlayerSprite _playerSprite;
     private Texture2D _wall;
     private Texture2D _floor;
+    private SpriteFont _font;
     private Texture2D _goal;
     private readonly int _texturesSize = 32;
+    private bool interfaceDrawn = false;
+    private int _menuIndex = 0;
+    private readonly string[] _menuItems = { "Start", "select Map Size", "Maze" };
     public MazeGame()
     {
         _graphics = new GraphicsDeviceManager(this);
@@ -32,17 +37,14 @@ public class MazeGame : Game
 
     protected override void Initialize()
     {
-        IMapProvider mapProvider = SelectMap();
+        IMapProvider mapProvider = new MazeGenerator(new MapVector(0,0), 1);
         _map = new Map(mapProvider);
-        _map.CreateMap();
+        _map.CreateMap(4, 4);
 
         base.Exiting += (sender, args) => logger.Info($"Game finished: game window is closed");
         logger.Info($"Player's starting position: x={_map.Player.StartX}, y={_map.Player.StartY}");
         logger.Info($"Goal's position: x={_map.Goal.X}, y={_map.Goal.Y}");
 
-        _graphics.PreferredBackBufferWidth = _map.Width * _texturesSize;
-        _graphics.PreferredBackBufferHeight = _map.Height * _texturesSize;
-        _graphics.ApplyChanges();
 
         _playerSprite = new PlayerSprite(this, _map.Player);
         this.Components.Add(_playerSprite);
@@ -66,10 +68,12 @@ public class MazeGame : Game
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
+        this.
         _goal = Content.Load<Texture2D>("Tree");
         _wall = Content.Load<Texture2D>("wall");
         _floor = Content.Load<Texture2D>("path");
-        DrawMap();
+        _font = Content.Load<SpriteFont>("font");
+        //DrawMap();
         base.LoadContent();
     }
 
@@ -80,40 +84,8 @@ public class MazeGame : Game
             logger.Info("Game finished: player has quit");
             Exit();
         }
-        if (_map.IsGameFinished)
-        {
-            logger.Info("Game finished: player has reached the goal");
-            Exit();
-        }
 
         base.Update(gameTime);
     }
 
-    protected override void Draw(GameTime gameTime)
-    {
-        base.Draw(gameTime);
-    }
-    private void DrawMap()
-    {
-        Block[,] grid = _map.MapGrid;
-        _spriteBatch.Begin();
-        for (int y = 0; y < grid.GetLength(0); y++)
-        {
-            for (int x = 0; x < grid.GetLength(1); x++)
-            {
-                if (grid[y, x] == Block.Solid)
-                {
-                    logger.Debug($"Draw wall at {x},{y}");
-                    _spriteBatch.Draw(_wall, new Vector2(x * _wall.Width, y * _wall.Height), Color.White);
-                }
-                else
-                {
-                    logger.Debug($"Draw path at {x},{y}");
-                    _spriteBatch.Draw(_floor, new Vector2(x * _floor.Width, y * _floor.Height), Color.White);
-                }
-            }
-        }
-        _spriteBatch.Draw(_goal, new Vector2(_map.Goal.X * _goal.Width, _map.Goal.Y * _goal.Height), Color.White);
-        _spriteBatch.End();
-    }   
-}
+
---
+++ MazeGame/MazeScreen.cs
@@ -0,0 +1,112 @@
+﻿using Maze;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using NLog;
+using System;
+using ButtonState = Microsoft.Xna.Framework.Input.ButtonState;
+using Keys = Microsoft.Xna.Framework.Input.Keys;
+
+namespace MazeGame
+{
+    internal class MazeScreen : DrawableGameComponent
+    {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+        private readonly GraphicsDeviceManager _graphics;
+        private readonly IMapProvider _mapProvider;
+        private readonly int? _width, _heigth;
+        private readonly int _texturesSize = 32;
+        private readonly Action _back;
+        private SpriteBatch _spriteBatch;
+        private Game game;
+        private IMap _map;
+        private PlayerSprite _playerSprite;
+        private Texture2D _wall;
+        private Texture2D _floor;
+        private Texture2D _goal;
+
+        public MazeScreen(Game game, IMapProvider mapProvider, Action back, int? width, int? heigth) : base(game)
+        {
+            this.game = game;
+            _mapProvider = mapProvider;
+            _back = back;
+            _width = width;
+            _heigth = heigth;
+
+        }
+
+
+        public override void Initialize()
+        {
+            _map = new Map(_mapProvider);
+            if (_width.HasValue && _heigth.HasValue)
+            {
+                _map.CreateMap(_width.Value, _heigth.Value);
+            }
+            else
+            {
+                _map.CreateMap();
+            }
+            logger.Info($"Player's starting position: x={_map.Player.StartX}, y={_map.Player.StartY}");
+            logger.Info($"Goal's position: x={_map.Goal.X}, y={_map.Goal.Y}");
+            _graphics.PreferredBackBufferWidth = _map.Width * _texturesSize;
+            _graphics.PreferredBackBufferHeight = _map.Height * _texturesSize;
+            _graphics.ApplyChanges();
+
+            _playerSprite = new PlayerSprite(game, _map.Player);
+            game.Components.Add(_playerSprite);
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+
+            _spriteBatch = new SpriteBatch(game.GraphicsDevice);
+            this.
+            _goal = game.Content.Load<Texture2D>("Tree");
+            _wall = game.Content.Load<Texture2D>("wall");
+            _floor = game.Content.Load<Texture2D>("path");
+            DrawMap();
+            base.LoadContent();
+        }
+        public override void Update(GameTime gameTime)
+        {
+            _playerSprite.Update(gameTime);
+            if (_map.IsGameFinished)
+            {
+                if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                {
+                    logger.Info("Game finished: player has quit");
+                    game.Exit();
+                }
+                logger.Info("Game finished: player has reached the goal");
+                _back();
+            }
+        }
+
+        private void DrawMap()
+        {
+            Block[,] grid = _map.MapGrid;
+            _spriteBatch.Begin();
+            for (int y = 0; y < grid.GetLength(0); y++)
+            {
+                for (int x = 0; x < grid.GetLength(1); x++)
+                {
+                    if (grid[y, x] == Block.Solid)
+                    {
+                        logger.Debug($"Draw wall at {x},{y}");
+                        _spriteBatch.Draw(_wall, new Vector2(x * _wall.Width, y * _wall.Height), Color.White);
+                    }
+                    else
+                    {
+                        logger.Debug($"Draw path at {x},{y}");
+                        _spriteBatch.Draw(_floor, new Vector2(x * _floor.Width, y * _floor.Height), Color.White);
+                    }
+                }
+            }
+            _spriteBatch.Draw(_goal, new Vector2(_map.Goal.X * _goal.Width, _map.Goal.Y * _goal.Height), Color.White);
+            _spriteBatch.End();
+        }
+
+    }
+}
