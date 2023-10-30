e10b93d4b466ad46c1ae6bd4d3b53301ddd9bdd9
clovis boucher <clovis.boucher@dawsoncollege.qc.ca>
2023-10-30 09:12:20 -0400

    remove unused import

--- MazeGame/InputManager.cs
+++ MazeGame/InputManager.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework.Input;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NLog;
 
 namespace MazeGame
