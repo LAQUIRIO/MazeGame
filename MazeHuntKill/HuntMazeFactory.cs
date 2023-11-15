44e1e9633b9e5e2d0fe119ead489440d0b7bc28b
clovis boucher <clovis.boucher@dawsoncollege.qc.ca>
2023-11-15 09:55:32 -0500

    add Hunt Factory

---
+++ MazeHuntKill/HuntMazeFactory.cs
@@ -0,0 +1,12 @@
+﻿using Maze;
+
+namespace MazeHuntKill
+{
+    public static class HuntMazeFactory
+    {
+        public static IMapProvider Create()
+        {
+            return new MazeHuntKillGenerator(null);
+        }
+    }
+}
